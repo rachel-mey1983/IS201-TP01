1eb0bcc1e4ca7704a8d03e389e7a65a9856b2514
ItsAMetric <150080893+rachel-mey1983@users.noreply.github.com>
2025-01-19 19:46:32 +0000

    PRIME

--- MOD2/prime_number.py
+++ MOD2/prime_number.py
@@ -1,20 +1,17 @@
 def is_prime(number):
-    
-    
     if number <= 1:
         return False
-    if number == 2 or number == 3:
-        return True
-    if number % 2 == 0 or number % 3 == 0:
-        return False
-    for i in range(5, int(number**0.5) + 1, 6):
-        if number % i == 0 or number % (i + 2) == 0:
+    for i in range(2, int(number / 2) + 1):
+        if number % i == 0:
             return False
     return True
 
 def main():
     num = int(input("Enter a number: "))
-    print(f"{num} is {'a' if is_prime(num) else 'not a'} prime number")
+    if is_prime(num):
+        print(f"{num} is a prime number")
+    else:
+        print(f"{num} is not a prime number")
 
 if __name__ == "__main__":
     main()
