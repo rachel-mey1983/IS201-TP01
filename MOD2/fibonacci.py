25bbaeff619792b50142b61c5051e288194948cd
ItsAMetric <150080893+rachel-mey1983@users.noreply.github.com>
2025-01-19 20:36:01 +0000

    2 COMMITS

--- MOD2/fibonacci.py
+++ MOD2/fibonacci.py
@@ -22,18 +22,20 @@
 # Input	
 
 def fibonacci(n):
-	if n <= 0:
-		return []
-	elif n == 1:
-		return [0]
-	elif n == 2:
-		return [0, 1]
-	fib_sequence = [0, 1]
-	for i in range(2, n):
-		fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
-	return fib_sequence
+    if n <= 0:
+        return []
+    if n == 1:
+        return [0]
+    if n == 2:
+        return [0, 1]
+    
+    fib_sequence = [0, 1]
+    for _ in range(2, n):
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    return fib_sequence
 
 num = int(input("How many Fibonacci numbers to generate? "))
 print(fibonacci(num))
+
 
 # Output
