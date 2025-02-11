85e18f8d334a5006cb448eb563370bff402d9df4
ItsAMetric <rachel.mey1983@gmail.com>
2025-02-10 19:40:43 -0800

    CREATEBOARD

--- TeamProject/TP01RachelM.py
+++ TeamProject/TP01RachelM.py
@@ -1,12 +1,12 @@
-####TICK TACK TOE####
-from typing import Any
+####TICK TACK TOE###
 
 print ( "Welcome to Tick Tack Toe!" )
 
 print("Rules of the Game")
 
+from gamecode import create_board
 
-def drawboard
+
 
 
 
--- TeamProject/gamecode.py
+++ TeamProject/gamecode.py
@@ -1,3 +1,3 @@
-def create_board():
+def create_board(numbers):
     board = "|1|2|3|\n|4|5|6|\n|7|8|9|"
-    print(board)
+    print(numbers)
