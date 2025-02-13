eb26666673d6fa688361e87aea4b1d7464ccf506
ItsAMetric <rachel.mey1983@gmail.com>
2025-02-13 08:23:36 -0800

    LIST

---
+++ Mod3/HOS03.py
@@ -0,0 +1,16 @@
+# HOS03.py - A program to manage a list of books in a library using a list of dictionaries  
+
+def add_books(book_list, book):
+    book_list.append(book)  # Add the book to the list  
+    print(f"Book '{book}' has been added to the list.") # Confirmation message  
+    
+# Example usage:
+
+def remove_books(book_list, book):
+    for book in book_list:  # Iterate through the list to find the book to remove
+        if book ['0'] == title:
+
+            book_list.remove(book)  # Remove the book from the list 
+            print(f"Removed book:{title}")  #  Confirmation message
+            
+        
